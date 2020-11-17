e86cdb9cf196467978e9a3b11d54ae16db7f9d2e
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2020-11-17 15:57:44 +0200

    1

---
+++ js/task-2.js
@@ -0,0 +1,36 @@
+// В HTML есть пустой список ul#ingredients.
+
+// < ul id = "ingredients" ></ul >
+//     В JS есть массив строк.
+
+// const ingredients = [
+//     'Картошка',
+//     'Грибы',
+//     'Чеснок',
+//     'Помидоры',
+//     'Зелень',
+//     'Приправы',
+// ];
+// Напиши скрипт, который для каждого элемента массива
+// ingredients создаст отдельный li, после чего вставит
+// все li за одну операцию в список ul.ingredients.
+// Для создания DOM - узлов используй document.createElement().
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const list = document.querySelector('#ingredients');
+
+list.prepend(...ingredients.map(createLi));
+
+function createLi(text) {
+  const el = document.createElement('li');
+  el.textContent = text;
+  return el;
+}
